0877e91d59ea975ff4302a2aef0e3b402a6ce3eb
Sirwan <sir1afifi@gmail.com>
2020-06-21 21:12:42 +0430

    Add login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import CloseIcon from "../components/CloseIcon";
 
 const Login = () => {
   return (
     <Container>
       <LoginBox>
+        <CloseIcon />
         <Header>Login to Continue</Header>
         <Form>
           <FormGroup>
@@ -15,6 +17,8 @@ const Login = () => {
             <label>Password</label>
             <input type="password" />
           </FormGroup>
+          <span>Forgot Password?</span>
+          <button>Login</button>
         </Form>
       </LoginBox>
     </Container>
@@ -32,26 +36,68 @@ const Container = styled.div`
 `;
 
 const LoginBox = styled.div`
+  position: relative;
   width: 500px;
   height: 600px;
   background: #ffffff;
   box-shadow: 0 14px 53px 0 rgba(84, 85, 144, 0.16);
   border-radius: 20px;
+
+  svg {
+    position: absolute;
+    right: 30px;
+    top: 30px;
+
+    :hover {
+      cursor: pointer;
+    }
+  }
 `;
 
 const Header = styled.div`
   font-size: 30px;
+  font-weight: lighter;
   color: #245373;
   letter-spacing: 0;
   text-align: center;
+  margin-top: 40px;
 `;
 
 const Form = styled.div`
   padding: 40px;
+
+  span {
+    display: block;
+    font-size: 18px;
+    color: #1997f0;
+    letter-spacing: 0;
+    text-align: center;
+  }
+
+  button {
+    width: 100%;
+    height: 79px;
+    background: #1997f0;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #ffffff;
+    letter-spacing: 0;
+    margin-top: 30px;
+    border: none;
+
+    :focus {
+      outline: none;
+    }
+
+    :hover {
+      cursor: pointer;
+      background: #0c78c4;
+    }
+  }
 `;
 
 const FormGroup = styled.div`
-  margin: 10px 0;
+  margin: 30px 0;
   label {
     display: block;
     font-size: 18px;
@@ -61,12 +107,19 @@ const FormGroup = styled.div`
 
   input {
     background: #ffffff;
+    color: #0d3c5c;
     border: 1px solid #e5e5e5;
     border-radius: 5px;
     width: 100%;
     height: 75px;
-    font-size: 30px;
+    font-size: 25px;
+    font-weight: lighter;
     margin-top: 10px;
     padding: 10px;
+
+    :focus {
+      outline: none;
+      border-bottom: 5px solid #1997f0;
+    }
   }
 `;
